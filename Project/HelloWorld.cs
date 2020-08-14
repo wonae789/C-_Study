e9061aec270af2ed1cc68fca6876c10718450f60
hoon <wonae789@naver.com>
2020-08-14 23:16:37 +0900

    c# first

---
+++ Project/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Project
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : HelloWorld.exe <이름>");
+                return;
+            }
+            WriteLine("Hello,{0}!", args[0]);
+        }
+    }
+}
